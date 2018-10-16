f7933fab7a46d0e1369044701780a0cc38006ee5
Greta-mart <ttanya1476@gmil.com>
2018-10-16 21:21:06 +0300

    add goal to pom

---
+++ Core/src/main/java/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        //just comment
+        int a = 4;
+        int b = 0;
+        try{
+            int c = a/b;
+        }
+        catch(Exception ex) {
+            System.out.println("Incorrect data");
+        }
+    }
+
+}
